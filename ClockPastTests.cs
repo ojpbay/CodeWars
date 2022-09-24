da3adcf2a6141ac35b87322b3ed8a575494fc097
ojpbay <oli.baylis@live.co.uk>
2022-09-24 07:26:30 +0100

    human readable time, bit hacky

--- ClockPastTests.cs
+++ ClockPastTests.cs
@@ -1,18 +1,55 @@
+using System;
 using NUnit.Framework;
 
 namespace CodeWars
 {
-    public class ClockPastTests
+    public static class TimeFormat
     {
-        [SetUp]
-        public void Setup()
+        public static string GetReadableTime(int seconds)
         {
+            TimeSpan ts = TimeSpan.FromSeconds(seconds);
+
+            if (ts.Days > 0)
+            {
+                return "99:59:59";
+            }
+            return ts.ToString(@"hh\:mm\:ss");
+        }
+    }
+
+
+
+    [TestFixture]
+    public class HumanReadableTimeTest
+    {
+        [Test]
+        public void HumanReadableTest_0()
+        {
+            Assert.AreEqual("00:00:00", TimeFormat.GetReadableTime(0));
         }
 
         [Test]
-        public void ClockPastTests1()
+        public void HumanReadableTest_5()
         {
-            Assert.AreEqual(61000, ClockPast.CountAfterMidnight(0, 1, 1));
+            Assert.AreEqual("00:00:05", TimeFormat.GetReadableTime(5));
+        }
+
+        [Test]
+        public void HumanReadableTest_60()
+        {
+            Assert.AreEqual("00:01:00", TimeFormat.GetReadableTime(60));
+        }
+
+        [Test]
+        public void HumanReadableTest_86399()
+        {
+            Assert.AreEqual("23:59:59", TimeFormat.GetReadableTime(86399));
+        }
+
+        [Test]
+        public void HumanReadableTest_359999()
+        {
+            Assert.AreEqual("99:59:59", TimeFormat.GetReadableTime(359999));
         }
     }
 }

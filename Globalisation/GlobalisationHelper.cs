0ea805e57e27ebd6c9c6f9a71921baa0a3dec7e1
ojpbay <oli.baylis@live.co.uk>
2024-01-29 17:24:06 +0000

    Localisation formatting tests

---
+++ Globalisation/GlobalisationHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace CodeWars
+{
+    public static class GlobalisationHelper
+    {
+        public static string GetLocalisedValue(double number, string locale)
+        {
+            return number.ToString("N", new CultureInfo(locale));
+        }
+    }
+}
---
+++ Globalisation/GlobalisationHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Globalisation
+{
+    [TestFixture]
+    public class GlobalisationHelperTests
+    {
+        [Theory]
+        [TestCase("en-GB", 1234567.89, "1,234,567.89")]
+        [TestCase("en-US", 1234567.89, "1,234,567.890")]
+        [TestCase("fr-FR", 1234567.89, "1 234 567,890")]
+        [TestCase("it-IT", 1234567.89, "1.234.567,890")]
+
+        public void GetLocalisedNumber(string locale, double number, string expected)
+        {
+            Assert.AreEqual(expected, GlobalisationHelper.GetLocalisedValue(number, locale));
+        }
+    }
+}

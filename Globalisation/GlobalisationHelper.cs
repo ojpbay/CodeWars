3549ab18ca5b4c0cc6e23785b1502e0cf64b9894
ojpbay <oli.baylis@live.co.uk>
2024-02-04 09:01:53 +0000

    Add tests for percentages with thousand separators which need to be divided by 100 once the percent symbol is in the format string

--- Globalisation/GlobalisationHelper.cs
+++ Globalisation/GlobalisationHelper.cs
@@ -4,9 +4,14 @@ namespace CodeWars
 {
     public static class GlobalisationHelper
     {
-        public static string GetLocalisedValue(double number, string locale)
+        public static string GetLocalisedValue(decimal number, string locale)
         {
             return number.ToString("N", new CultureInfo(locale));
+        }
+
+        public static string GetLocalisedPercentageValue(decimal number, string locale, string format)
+        {
+            return (number / 100).ToString(format, new CultureInfo(locale));
         }
     }
 }
--- Globalisation/GlobalisationHelperTests.cs
+++ Globalisation/GlobalisationHelperTests.cs
@@ -11,9 +11,33 @@ namespace CodeWars.Globalisation
         [TestCase("fr-FR", 1234567.89, "1 234 567,890")]
         [TestCase("it-IT", 1234567.89, "1.234.567,890")]
 
-        public void GetLocalisedNumber(string locale, double number, string expected)
+        public void GetLocalisedNumber(string locale, decimal number, string expected)
         {
             Assert.AreEqual(expected, GlobalisationHelper.GetLocalisedValue(number, locale));
+        }
+
+        [Theory]
+        [TestCase("en-GB", 1234567.123456, "1234567.123456%")]
+        [TestCase("en-US", 1234567.123456, "1234567.123456%")]
+        [TestCase("fr-FR", 1234567.123456, "1234567,123456%")]
+        [TestCase("it-IT", 1234567.123456, "1234567,123456%")]
+        [TestCase("pt-BR", 1234567.123456, "1234567,123456%")]
+
+        public void GetLocalisedPercentageNumber(string locale, decimal number, string expected)
+        {
+            Assert.AreEqual(expected, GlobalisationHelper.GetLocalisedPercentageValue(number, locale, "#.######%"));
+        }
+
+        [Theory]
+        [TestCase("en-GB", 1234567.123456, "1,234,567.123456%")]
+        [TestCase("en-US", 1234567.123456, "1,234,567.123456%")]
+        [TestCase("fr-FR", 1234567.123456, "1 234 567,123456%")]
+        [TestCase("it-IT", 1234567.123456, "1.234.567,123456%")]
+        [TestCase("pt-BR", 1234567.123456, "1.234.567,123456%")]
+
+        public void GetLocalisedPercentageNumberWithThousandSeparator(string locale, decimal number, string expected)
+        {
+            Assert.AreEqual(expected, GlobalisationHelper.GetLocalisedPercentageValue(number, locale, "#,###.######%"));
         }
     }
 }

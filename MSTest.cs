b545d109d841b513850d88816810b52ff5aa4a7e
Oliver Baylis <1391064+ojpbay@users.noreply.github.com>
2024-05-21 10:07:26 +0100

    Rename tests to be meaningful

--- MSTest.cs
+++ MSTest.cs
@@ -28,7 +28,7 @@ namespace CodeWars
             return iterations;
         }
 
-        public BigInteger ConvertToBigInt(string s)
+        public static BigInteger ConvertToBigInt(string s)
         {
             return s.Aggregate(BigInteger.Zero, (acc, c) => acc * 2 + (c == '1' ? 1 : 0));
         }
@@ -43,47 +43,43 @@ namespace CodeWars
     public class MSTest
     {
         [Test]
-        public void TestToBigInt1()
-        {
-            var s = "1000001111";
-            var sol = new Solution();
-            var result = sol.ConvertToBigInt(s);
-
-            Assert.That(result == 527);
-        }
-
-        [Test]
-        public void TestToBigInt2()
-        {
-            var s = "011100";
-            var sol = new Solution();
-            var result = sol.ConvertToBigInt(s);
-
-            Assert.That(result == 28);
-        }
-
-        [Test]
-        public void TestToBigInt3()
+        public void ConvertToBigInt_Binary11_ReturnsDecimal3()
         {
             var s = "11";
-            var sol = new Solution();
-            var result = sol.ConvertToBigInt(s);
+            var result = Solution.ConvertToBigInt(s);
 
             Assert.That(result == 3);
         }
 
         [Test]
-        public void TestToBigInt4()
+        public void ConvertToBigInt_Binary10_ReturnsDecimal2()
         {
             var s = "10";
-            var sol = new Solution();
-            var result = sol.ConvertToBigInt(s);
+            var result = Solution.ConvertToBigInt(s);
 
             Assert.That(result == 2);
         }
 
         [Test]
-        public void Test0()
+        public void ConvertToBigInt_Binary1000001111_ReturnsDecimal527()
+        {
+            var s = "1000001111";
+            var result = Solution.ConvertToBigInt(s);
+
+            Assert.That(result == 527);
+        }
+
+        [Test]
+        public void ConvertToBigInt_Binary011100_ReturnsDecimal28()
+        {
+            var s = "011100";
+            var result = Solution.ConvertToBigInt(s);
+
+            Assert.That(result == 28);
+        }
+
+        [Test]
+        public void GivenBinaryString1_Returns1InvocationToReach0()
         {
             var s = "1";
             var sol = new Solution();
@@ -93,7 +89,7 @@ namespace CodeWars
         }
 
         [Test]
-        public void Test1()
+        public void GivenBinaryString011100_Returns7InvocationsToReach0()
         {
             var s = "011100";
             var sol = new Solution();
@@ -103,7 +99,7 @@ namespace CodeWars
         }
 
         [Test]
-        public void Test2()
+        public void GivenBinaryString111_Returns5InvocationsToReach0()
         {
             var s = "111";
             var sol = new Solution();
@@ -114,7 +110,7 @@ namespace CodeWars
         }
 
         [Test]
-        public void Test3()
+        public void GivenBinaryString1111010101111_Returns22InvocationsToReach0()
         {
             var s = "1111010101111";
 
@@ -125,7 +121,7 @@ namespace CodeWars
         }
 
         [Test]
-        public void Test4()
+        public void GivenHugeBinaryString_ReturnsBigIntegerValue()
         {
             // append 1 to 400000 times
             var s = new string('1', 400000);

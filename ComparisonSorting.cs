f32f225a7292182404338642d2b3088f4e06e012
ojpbay <oli.baylis@live.co.uk>
2024-09-24 20:43:26 +0100

    Fix failing test

--- ComparisonSorting.cs
+++ ComparisonSorting.cs
@@ -30,7 +30,7 @@ namespace CodeWars
             var expected = new List<int> { 0, 2, 0, 2, 0, 0, 1, 0, 1, 2, 1, 0, 1, 1, 0, 0, 2, 0, 1, 0, 1, 2, 1, 1, 1, 3, 0, 2, 0, 0, 2, 0, 3, 3, 1, 0, 0, 0, 0, 2, 2, 1, 1, 1, 2, 0, 2, 0, 1, 0, 1, 0, 0, 1, 0, 0, 2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 3, 2, 0, 0, 2, 1, 2, 1, 0, 2, 2, 1, 2, 1, 2, 1, 1, 2, 2, 0, 3, 2, 1, 1, 0, 1, 1, 1, 0, 2, 2 };
 
             var result = ComparisonSortingResult.countingSort(input);
-            Assert.That(result, Is.EqualTo(4294967294));
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }

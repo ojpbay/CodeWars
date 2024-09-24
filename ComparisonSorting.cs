d49e61189b5c7ecb8e6eb1278cd62de76e35c1c0
ojpbay <oli.baylis@live.co.uk>
2024-09-24 20:43:04 +0100

    Failing test

---
+++ ComparisonSorting.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public static class ComparisonSortingResult
+    {
+        /*
+         * Complete the 'countingSort' function below.
+         * 
+         * see https://www.hackerrank.com/challenges/one-month-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static List<int> countingSort(List<int> arr)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class ComparisonSortingSolution
+    {
+        [Test]
+        public void Test1()
+        {
+            var input = new List<int> { 63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33 };
+            var expected = new List<int> { 0, 2, 0, 2, 0, 0, 1, 0, 1, 2, 1, 0, 1, 1, 0, 0, 2, 0, 1, 0, 1, 2, 1, 1, 1, 3, 0, 2, 0, 0, 2, 0, 3, 3, 1, 0, 0, 0, 0, 2, 2, 1, 1, 1, 2, 0, 2, 0, 1, 0, 1, 0, 0, 1, 0, 0, 2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 3, 2, 0, 0, 2, 1, 2, 1, 0, 2, 2, 1, 2, 1, 2, 1, 1, 2, 2, 0, 3, 2, 1, 1, 0, 1, 1, 1, 0, 2, 2 };
+
+            var result = ComparisonSortingResult.countingSort(input);
+            Assert.That(result, Is.EqualTo(4294967294));
+        }
+    }
+}

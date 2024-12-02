a4ed0c97ed5275a0d6fa6a1f738bb41de1364a90
ojpbay <oli.baylis@live.co.uk>
2024-12-02 17:14:54 +0000

    Remove unnecessary first iteration

--- BreakingTheRecords.cs
+++ BreakingTheRecords.cs
@@ -16,13 +16,6 @@ namespace CodeWars
 
             for (int i = 1; i < scores.Count; i++)
             {
-                if (i == 0)
-                {
-                    min = scores[i];
-                    max = scores[i];
-                    continue;
-                }
-
                 if (scores[i] < min)
                 {
                     min = scores[i];
@@ -38,7 +31,6 @@ namespace CodeWars
 
             return new List<int>() { maxBrokenCount, minBrokenCount };
         }
-
     }
 
 
@@ -49,7 +41,7 @@ namespace CodeWars
         [Test]
         public void Test1()
         {
-            int[] integers = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+            int[] integers = { 10, 5, 20, 20, 4, 5, 2, 25, 1 };
 
             var result = BreakTheRecordsResult.breakingRecords(integers.ToList());
             Assert.That(result[0], Is.EqualTo(2)); // number of times highest score was broken
@@ -59,7 +51,7 @@ namespace CodeWars
         [Test]
         public void Test2()
         {
-            int[] integers = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+            int[] integers = { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 };
 
             var result = BreakTheRecordsResult.breakingRecords(integers.ToList());
             Assert.That(result[0], Is.EqualTo(4));

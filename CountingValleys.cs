abbb5ef2d20aca1b6d0fefcc4f86bb40ed0bd053
ojpbay <oli.baylis@live.co.uk>
2024-12-11 16:55:33 +0000

    Add countingValleys method and unit test
    - Added the `NUnit.Framework` using directive to include the NUnit testing framework.
    - Introduced a new namespace `CodeWars` to encapsulate the code.
    - Created a static class `CountingValleysResult` containing the `countingValleys` method, which calculates the number of valleys in a given path.
    - Implemented the `countingValleys` method to track the hiker's current depth, minimum depth, and count the number of valleys traversed.
    - Added a `TestFixture` class `CountingValleysSolution` with a test method `TestOfSingleValley` to verify the functionality of the `countingValleys` method using NUnit.
    - The test method `TestOfSingleValley` checks if the `countingValleys` method correctly identifies a single valley in the path "UDDDUDUU".

---
+++ CountingValleys.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars
+{
+    public static class CountingValleysResult
+    {
+        /*
+         * Complete the 'countingValleys' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER steps
+         *  2. STRING path
+         */
+
+        public static int countingValleys(int steps, string path)
+        {
+            int currentDepth = 0;
+            int minDepth = 0;
+            int valleyCount = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (path.Substring(i, 1).ToLowerInvariant() == "d")
+                {
+                    currentDepth--;
+                }
+                else
+                {
+                    currentDepth++;
+                }
+
+                if (currentDepth < minDepth)
+                {
+                    minDepth = currentDepth;
+                }
+
+                if (currentDepth == 0 && minDepth < 0)
+                {
+                    valleyCount++;
+                    minDepth = currentDepth;
+                }
+            }
+
+            return valleyCount;
+        }
+    }
+
+
+
+    [TestFixture]
+    public class CountingValleysSolution
+    {
+        [Test]
+        public void TestOfSingleValley()
+        {
+            var path = "UDDDUDUU";
+
+            var result = CountingValleysResult.countingValleys(path.Length, path);
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
